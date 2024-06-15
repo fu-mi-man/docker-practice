33320cc9cb23a7dec9eb15abf8f3d57169768d38
fu-mi-man <fumichan0393@gmail.com>
2024-06-15 10:21:31 +0900

    mysql container

---
+++ mysql/script/init.sql
@@ -0,0 +1,12 @@
+USE db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO
+    users (username, email)
+VALUES
+    ('test', 'test@example.com');
